4a4b3774efba55579093413337a3e9c521d88da5
Troy <109938516@BIS.AD.UCC.IE>
2019-02-26 19:20:15 +0000

    I'm submitting all my php files

---
+++ index.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<TITLE>Multi-page Form - Page One</title>
+<HEAD>
+
+<body>
+<p>Please fill in the following information</p>
+
+<FORM METHOD="POST" ACTION="page2.php">
+Name:<INPUT TYPE="text" SIZE="40" name="cust_name"><BR>
+Email:<INPUT TYPE="text" SIZE="40" name="cust_email"><BR>
+<INPUT TYPE="submit" name="submit1" value="Proceed">
+</FORM>
+
+</BODY>
+</HTML>
